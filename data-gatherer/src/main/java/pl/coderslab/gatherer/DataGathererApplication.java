8d853d48cef24e50ad958e90f7a43cf19302bc6c
Weronika Paliniewicz <weronika.paliniewicz@autopay.pl>
2025-09-26 20:28:59 +0200

    add default configuration for exchange rates and messaging

--- data-gatherer/src/main/java/pl/coderslab/gatherer/DataGathererApplication.java
+++ data-gatherer/src/main/java/pl/coderslab/gatherer/DataGathererApplication.java
@@ -2,11 +2,16 @@ package pl.coderslab.gatherer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import pl.coderslab.gatherer.config.ExchangeRateProperties;
+import pl.coderslab.gatherer.config.MessagingProperties;
 
-@SpringBootApplication(scanBasePackages = "pl.coderslab.gatherer")
+@SpringBootApplication
 @EnableScheduling
+@EnableConfigurationProperties({ExchangeRateProperties.class, MessagingProperties.class})
 public class DataGathererApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(DataGathererApplication.class, args);
     }
---
+++ data-gatherer/src/main/java/pl/coderslab/gatherer/config/MessagingProperties.java
@@ -0,0 +1,35 @@
+package pl.coderslab.gatherer.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "app.messaging")
+public class MessagingProperties {
+
+    private String exchange = "currency.alerts.exchange";
+    private String queue = "currency.alerts.queue";
+    private String routingKey = "currency.alerts.rate-change";
+
+    public String getExchange() {
+        return exchange;
+    }
+
+    public void setExchange(String exchange) {
+        this.exchange = exchange;
+    }
+
+    public String getQueue() {
+        return queue;
+    }
+
+    public void setQueue(String queue) {
+        this.queue = queue;
+    }
+
+    public String getRoutingKey() {
+        return routingKey;
+    }
+
+    public void setRoutingKey(String routingKey) {
+        this.routingKey = routingKey;
+    }
+}
