72302dd11996d1c78d679afd4c5524029c8b0006
Weronika Paliniewicz <weronika.paliniewicz@autopay.pl>
2025-09-27 12:08:50 +0200

    added implementation of CurrencyRateService and unit tests for CurrencyRateListener class

--- data-provider/src/main/java/pl/coderslab/provider/controller/CurrencyRateController.java
+++ data-provider/src/main/java/pl/coderslab/provider/controller/CurrencyRateController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.coderslab.provider.entity.CurrencyRate;
-import pl.coderslab.provider.repository.CurrencyRateRepository;
 import pl.coderslab.provider.service.CurrencyRateService;
 
 import java.util.List;
--- data-provider/src/main/java/pl/coderslab/provider/listener/CurrencyRateListener.java
+++ data-provider/src/main/java/pl/coderslab/provider/listener/CurrencyRateListener.java
@@ -1,32 +1,33 @@
 package pl.coderslab.provider.listener;
 
-import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import pl.coderslab.currencyalert.common.messaging.CurrencyRateChangeMessage;
-import pl.coderslab.provider.entity.CurrencyRate;
+import pl.coderslab.provider.service.CurrencyRateService;
 import pl.coderslab.provider.repository.CurrencyRateRepository;
 
-import java.time.ZoneOffset;
-
 @Service
-@RequiredArgsConstructor
 public class CurrencyRateListener {
 
     private static final Logger log = LoggerFactory.getLogger(CurrencyRateListener.class);
 
-    private final CurrencyRateRepository repository;
+    private final CurrencyRateService currencyRateService;
+
+    @Autowired
+    public CurrencyRateListener(CurrencyRateService currencyRateService) {
+        this.currencyRateService = currencyRateService;
+    }
+
+    public CurrencyRateListener(CurrencyRateRepository repository) {
+        this(new CurrencyRateService(repository));
+    }
 
     @RabbitListener(queues = "#{currencyAlertQueue.name}")
     public void receiveMessage(CurrencyRateChangeMessage message) {
-        CurrencyRate entity = new CurrencyRate();
-        entity.setCurrencyCode(message.currency());
-        entity.setRate(message.newRate());
-        entity.setTimestamp(message.timestamp().atOffset(ZoneOffset.UTC));
-
-        repository.save(entity);
+        currencyRateService.recordRateChange(message);
         log.debug("Stored rate update for {}", message.currency());
     }
 }
